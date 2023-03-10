feeab9dcc526820425fcf104144d8215e6f9c6da
Tim <timkin99@mail.ru>
2023-03-11 01:33:46 +0300

    fix logger

--- src/logger.py
+++ src/logger.py
@@ -4,7 +4,6 @@ import sys
 
 logging.basicConfig(format="%(asctime)s — %(name)s — %(levelname)s — %(filename)s/%(funcName)s:%(lineno)d — %(message)s",
                     encoding="utf-8",
-                    level="INFO",)
-handler = logging.StreamHandler(stream=sys.stdout)
+                    level="INFO",
+                    stream=sys.stdout)
 logger = logging.getLogger("MAIN_LOGGER")
-logger.addHandler(handler)
