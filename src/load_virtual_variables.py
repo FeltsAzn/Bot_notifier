34a15b53f7cbbef6af0bac31fbdd18c3daca42e9
Tim <timkin99@mail.ru>
2023-03-14 23:09:06 +0300

    change webhook path

--- src/load_virtual_variables.py
+++ src/load_virtual_variables.py
@@ -7,7 +7,6 @@ if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
 TOKEN = os.getenv("BOT_TOKEN")
-MULTIPROCESS_CONFIG = os.getenv("MULTIPROCESSORING")
 MAIN_ADMIN = int(os.getenv("SUPER_ADMIN_ID"))
 DATABASE_ASYNC_URL = os.getenv("DATABASE_URL_ASYNC")
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -15,7 +14,7 @@ WEBHOOK_PATH = os.getenv("WEBHOOK_PATH")
 WEBAPP_HOST = os.getenv("WEBAPP_HOST")
 BOT_PORT = os.getenv("BOT_PORT")
 DOMAIN = os.getenv("DOMAIN_NAME")
-WEBHOOK_URL = f"{DOMAIN}{WEBHOOK_PATH}"
+WEBHOOK_URL = f"{DOMAIN}/{WEBHOOK_PATH}/{TOKEN}"
 
 
 __redis_url = os.getenv("REDIS_URL")
