b8c2f030bcc0655ead8f92b087e856373700d1a8
Tim <timkin99@mail.ru>
2023-03-14 23:09:51 +0300

    fix logger output

--- src/logger.py
+++ src/logger.py
@@ -1,9 +1,7 @@
 import logging
-import sys
 
 
 logging.basicConfig(format="%(asctime)s — %(name)s — %(levelname)s — %(filename)s/%(funcName)s:%(lineno)d — %(message)s",
                     encoding="utf-8",
-                    level="INFO",
-                    stream=sys.stdout)
+                    level="INFO")
 logger = logging.getLogger("MAIN_LOGGER")
