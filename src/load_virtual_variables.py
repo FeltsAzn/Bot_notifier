de95335773ba36fce07dd006a5f476fb34dac4d4
Tim <timkin99@mail.ru>
2023-03-15 00:03:55 +0300

    deployment fixes

--- src/load_virtual_variables.py
+++ src/load_virtual_variables.py
@@ -7,14 +7,14 @@ if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
 TOKEN = os.getenv("BOT_TOKEN")
+BOT_PORT = os.getenv("BOT_PORT")
 MAIN_ADMIN = int(os.getenv("SUPER_ADMIN_ID"))
 DATABASE_ASYNC_URL = os.getenv("DATABASE_URL_ASYNC")
-DATABASE_URL = os.getenv("DATABASE_URL")
-WEBHOOK_PATH = os.getenv("WEBHOOK_PATH")
+
+WEBHOOK_PATH = f'{os.getenv("WEBHOOK_BASE_PATH")}/{TOKEN}'
 WEBAPP_HOST = os.getenv("WEBAPP_HOST")
-BOT_PORT = os.getenv("BOT_PORT")
 DOMAIN = os.getenv("DOMAIN_NAME")
-WEBHOOK_URL = f"{DOMAIN}/{WEBHOOK_PATH}/{TOKEN}"
+WEBHOOK_URL = f"{DOMAIN}{WEBHOOK_PATH}"
 
 
 __redis_url = os.getenv("REDIS_URL")
