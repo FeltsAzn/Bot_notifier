517df5ee02018d2b7a723eeea632466b18d83b92
Tim <timkin99@mail.ru>
2023-03-16 01:18:20 +0300

    main: add load base boundary volume

--- src/handlers/admin_handler/admin_panel.py
+++ src/handlers/admin_handler/admin_panel.py
@@ -1,8 +1,9 @@
 from aiogram.types import Message, InlineKeyboardButton, ReplyKeyboardMarkup
 from aiogram.dispatcher.filters import Text
-from loader import dp
+from loader import dp, VolumeState
 from handlers.admin_handler.config_for_filling import fill_table
-from middleware import admin_validator
+from middleware import admin_validator, set_value_to_redis
+from aiogram.dispatcher import FSMContext
 
 """
 Файл admin_panel.py - главный обработчик для доступа к админ-панели.
@@ -11,8 +12,8 @@ from middleware import admin_validator
 
 @dp.message_handler(Text(equals="Admin panel"))
 @admin_validator
-async def start(message: Message):
-    buttons = ['List of users', "Admins settings", 'Back to user menu']
+async def start_admin_panel(message: Message):
+    buttons = ['List of users', "Change minimal volume", "Admins settings", 'Back to user menu']
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
     keyboard.add(*buttons)
     await message.answer("Choose action:", reply_markup=keyboard)
@@ -30,5 +31,30 @@ async def start_list_of_users(message: Message):
         keyboard.insert(next_button)
     text = f"List of users (1/{fill_table.last_page}):"
     await message.answer(text, reply_markup=keyboard)
+
+
+@dp.message_handler(Text(equals="Change minimal volume"))
+@admin_validator
+async def start(message: Message):
+    buttons = ["Cancel set volume"]
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    keyboard.add(*buttons)
+    await message.answer("Set minimum value of volume, which will boundary value", reply_markup=keyboard)
+    await VolumeState.set_volume.set()
+
+
+@dp.message_handler(state=VolumeState.set_volume)
+async def set_volume(message: Message, state: FSMContext):
+    if message.text == "Cancel set volume":
+        await state.reset_state()
+        return await start_admin_panel(message)
+
+    is_right_volume = await set_value_to_redis(message.text)
+    if is_right_volume:
+        await state.reset_state()
+        await message.answer(f"Boundary volume {message.text} set successfully")
+        return await start_admin_panel(message)
+    else:
+        await message.answer(f"Value {message.text} is wrong, set right value")
 
 
--- src/loader.py
+++ src/loader.py
@@ -1,7 +1,14 @@
 from aiogram import Dispatcher, Bot
 from asyncio import get_event_loop
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from load_virtual_variables import TOKEN
 
 
+class VolumeState(StatesGroup):
+    set_volume = State()
+
+
 bot = Bot(token=TOKEN, parse_mode="HTML")
-dp = Dispatcher(bot, loop=get_event_loop())
+storage = MemoryStorage()
+dp = Dispatcher(bot, loop=get_event_loop(), storage=storage)
