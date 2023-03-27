7b1dbe1f79de1706c5c1d93588daa37c94298c2f
Tim <timkin99@mail.ru>
2023-03-27 23:48:30 +0300

    main: added menu for changing volume jumps

--- src/handlers/admin_handler/change_filter_settings.py
+++ src/handlers/admin_handler/change_filter_settings.py
@@ -9,19 +9,23 @@ from aiogram.dispatcher import FSMContext
 @admin_validator
 async def show_settings(message: Message):
     settings = await get_settings()
-    volume = InlineKeyboardButton(text=f"Volume $: {settings['volume']}",
+    volume = InlineKeyboardButton(text=f"Volume: {settings['volume']}$",
                                   callback_data="volume:change:call")
-    start_percent = InlineKeyboardButton(text=f"Start %: {settings['start']}",
+    start_percent = InlineKeyboardButton(text=f"Start: {settings['start']}%",
                                          callback_data="start:change:call")
-    up_percent = InlineKeyboardButton(text=f"Up step %: {settings['up']}",
+    up_percent = InlineKeyboardButton(text=f"Up step: {settings['up']}%",
                                       callback_data="up:change:call")
-    down_percent = InlineKeyboardButton(text=f"Down step %: {settings['down']} ",
+    down_percent = InlineKeyboardButton(text=f"Down step: {settings['down']}%",
                                         callback_data="down:change:call")
-    high_percent = InlineKeyboardButton(text=f"High %: {settings['high']}",
+    high_percent = InlineKeyboardButton(text=f"High percent: {settings['high']}%",
                                         callback_data="high:change:call")
+    volume_15_min = InlineKeyboardButton(text=f"Volume jump 15 min: {settings['volume_15_min']}%",
+                                         callback_data="volume_15_min:change:call")
+    volume_30_min = InlineKeyboardButton(text=f"Volume jump 30 min: {settings['volume_30_min']}%",
+                                         callback_data="volume_30_min:change:call")
 
-    buttons = [volume, start_percent, up_percent, down_percent, high_percent]
-    keyboard = InlineKeyboardMarkup(row_width=2)
+    buttons = [volume, start_percent, up_percent, down_percent, high_percent, volume_15_min, volume_30_min]
+    keyboard = InlineKeyboardMarkup(row_width=1)
     keyboard.add(*buttons)
     await message.answer("Values:", reply_markup=keyboard)
 
